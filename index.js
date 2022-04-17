5752febc646fe55c86a4c56f3a868d259f72a96b
Denisha Garner <thegarnercode@gmail.com>
2022-04-17 15:33:52 -0400

    commit tree 8f6d2919071cf7fc1c510bf0d7be30491036db6a

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solutions in this file
+const array =[ "Guadalupe","Ollie","Aki"]
+
+
+function writeCards(names, occasion){
+    
+    let messageNames=[];
+    for ( let i = 0; i < names.length; i++){
+        let names = array[i];
+        let message = `Thank You, ${names}, for the wonderful ${occasion} gift!`
+                    messageNames.push(message);
+        
+    }
+    return message;
+}
+
+const countDown = 11;
+while (countDown > 10){
+    console.log(countDown--)
+}
+
+
+
+
